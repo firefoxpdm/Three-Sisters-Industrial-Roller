ef0fdf99759fca2eea4dac113c470180d6163a35
Tamas.Szekeres <firefoxpdm@yahoo.co.uk>
2020-03-21 13:19:36 +0100

    1.1 baseline

---
+++ Source/ThreeSistersIndustrialRoller/Controller.cs
@@ -0,0 +1,58 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace ThreeSistersIndustrialRoller
+{
+    class Controller : Mod
+    {
+        private static Harmony harmony;
+        public Controller(ModContentPack content) : base(content)
+        {
+            harmony = new Harmony("com.firefox.threesistersindustrialroller");
+            harmony.Patch(
+                original: AccessTools.Method("RimWorldIndustrialRollers.MovingRailGreenPuller:DoTickerWork"),
+                transpiler: new HarmonyMethod(
+                    methodType: typeof(Controller),
+                    methodName: nameof(patchGreenPullerWork))
+                );
+        }
+
+        static IEnumerable<CodeInstruction> patchGreenPullerWork(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode.Name.Equals("ldftn"))
+                {
+                    harmony.Patch(
+                        original: codes[i].operand as MethodInfo,
+                        transpiler: new HarmonyMethod(
+                            methodType: typeof(Controller),
+                            methodName: nameof(patchGreenPullerWorkInternal)
+                        ));
+                }
+            }
+            return codes.AsEnumerable();
+        }
+
+        static IEnumerable<CodeInstruction> patchGreenPullerWorkInternal(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode.Name.Equals("callvirt"))
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Call, typeof(ThreeSistersResolver).GetMethod("ItemMatchesGrowthZonePlantProduce"));
+                }
+                
+            }
+            return codes.AsEnumerable();
+        }
+
+   
+    }
+}
---
+++ Source/ThreeSistersIndustrialRoller/ThreeSistersResolver.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThreeSistersIndustrialRoller
+{
+    public class ThreeSistersResolver
+    {
+        static readonly Dictionary<string, ISet<string>> plantsProduces = new Dictionary<string, ISet<string>>();
+
+        static ThreeSistersResolver()
+        {
+            add("BcLS_SistersAItemSpawner", "RawBlueCorn", "RawRedLentil", "Rawsquash");
+            add("CLS_SistersAItemSpawner", "RawCorn", "RawRedLentil", "Rawsquash");
+            add("CLP_SistersAItemSpawner", "RawCorn", "RawRedLentil", "Rawpumpkin");
+            add("CBS_SistersAItemSpawner", "RawCorn", "Rawbean", "Rawsquash");
+            add("CBP_SistersAItemSpawner", "RawCorn", "Rawbean", "Rawpumpkin");
+            add("BcLP_SistersAItemSpawner", "RawBlueCorn", "RawRedLentil", "Rawpumpkin");
+            add("BcBS_SistersAItemSpawner", "RawBlueCorn", "Rawbean", "Rawsquash");
+            add("BcBP_SistersAItemSpawner", "RawBlueCorn", "Rawbean", "Rawpumpkin");
+
+            add("PLS_SistersAItemSpawner", "Rawpeach", "RawRedLentil", "RawBerries");
+            add("PBS_SistersAItemSpawner", "Rawpeach", "Rawbean", "RawBerries");
+            add("ALS_SistersAItemSpawner", "Rawapple", "RawRedLentil", "RawBerries");
+            add("ABS_SistersAItemSpawner", "Rawapple", "Rawbean", "RawBerries");
+
+            add("Pomato_SistersAItemSpawner", "RawTomatoes", "RawPotatoes");
+        }
+
+        private static void add(string plant, params string[] products)
+        {
+            ISet<string> set = new HashSet<string>();
+            products.All(product => set.Add(product));
+            plantsProduces.Add(plant, set);
+        }
+
+        public static bool ItemMatchesGrowthZonePlantProduce(string item, string plantProduce)
+        {
+            if (item.Equals(plantProduce))
+            {
+                return true;
+            }
+
+            ISet<string> set;
+            plantsProduces.TryGetValue(plantProduce, out set);
+            if (set == null)
+            {
+                return false;
+            }
+            bool result = set.Contains(item);
+            return result;
+        }
+    }
+}
